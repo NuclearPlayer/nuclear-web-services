f1bb7dfd8f7c215939edfb679f0fd2acaf46e531
nukeop <nukeop@gumblert.tech>
2021-06-23 00:47:07 +0200

    Move db setup to core

---
+++ packages/core/src/database/index.ts
@@ -0,0 +1,33 @@
+import { Sequelize } from 'sequelize-typescript';
+
+import { Logger } from '..';
+import { Environment } from '../types';
+import { config } from './config';
+
+const env: Environment = (process.env.NODE_ENV || 'development') as Environment;
+let sequelize: Sequelize;
+
+if (env === 'production') {
+  sequelize = new Sequelize(config[env].database as string, {
+    dialect: config[env].dialect as any,
+    protocol: config[env].protocol,
+    dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+  });
+} else {
+  sequelize = new Sequelize({
+    dialect: config[env].dialect as any,
+    storage: config[env].database as string,
+    logging: env !== 'test',
+  });
+}
+
+sequelize
+  .authenticate()
+  .then(() => {
+    Logger.info('🟢 The database is connected.');
+  })
+  .catch((error: Error) => {
+    Logger.info(`🔴 Unable to connect to the database: ${error}.`);
+  });
+
+export { sequelize };
--- packages/identity/src/database/index.ts
+++ packages/identity/src/database/index.ts
@@ -1,36 +1,7 @@
-import { Sequelize } from 'sequelize-typescript';
-import { Environment } from '@nws/core/src/types';
-import { Logger } from '@nws/core';
+import { sequelize } from '@nws/core/src/database';
 
 import { Group } from '../models/groups.model';
 import { User } from '../models/users.model';
-import { config } from './config';
-
-const env: Environment = (process.env.NODE_ENV || 'development') as Environment;
-let sequelize: Sequelize;
-
-if (env === 'production') {
-  sequelize = new Sequelize(config[env].database as string, {
-    dialect: config[env].dialect as any,
-    protocol: config[env].protocol,
-    dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
-  });
-} else {
-  sequelize = new Sequelize({
-    dialect: config[env].dialect as any,
-    storage: config[env].database as string,
-    logging: env !== 'test',
-  });
-}
-
-sequelize
-  .authenticate()
-  .then(() => {
-    Logger.info('🟢 The database is connected.');
-  })
-  .catch((error: Error) => {
-    Logger.info(`🔴 Unable to connect to the database: ${error}.`);
-  });
 
 sequelize.addModels([User, Group]);
 
