a8c2229a7989822492b7ae437538dcf065acd570
nukeop <nukeop@gumblert.tech>
2021-07-05 00:16:14 +0200

    Use tsc instead of webpack

--- packages/playlists/src/adapters/identity-service.adapter.ts
+++ packages/playlists/src/adapters/identity-service.adapter.ts
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 export class IdentityServiceAdapter {
   getUser = async (id: string, authorization: string) =>
     (
-      await fetch(`${process.env.identityServiceUrl}/users/${id}`, {
+      await fetch(`${process.env.IDENTITY_SERVICE_URL}/users/${id}`, {
         headers: { authorization },
       })
     ).json();
