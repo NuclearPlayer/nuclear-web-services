5b32dc92271c3f137ca968c9ca7ee28ffbfe0d43
nukeop <nukeop@gumblert.tech>
2021-09-01 02:07:50 +0200

    Re-enable ssl in postgres

--- packages/core/src/database/index.ts
+++ packages/core/src/database/index.ts
@@ -12,6 +12,7 @@ if (env === 'production') {
   sequelize = new Sequelize(config[env].database as string, {
     dialect: config[env].dialect as any,
     protocol: config[env].protocol,
+    dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
   });
 } else {
   sequelize = new Sequelize({
